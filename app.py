f454c5947b2a87c6d2407f6dff5f4f7df1ca6bf5
terrilevi <118628583+terrilevi@users.noreply.github.com>
2024-10-04 17:09:23 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,15 +4,47 @@ import csv
 from datetime import datetime
 import random
 
-st.title("a little chat")
+import streamlit as st
+import random
+import time
 
-try:
-    import openai
-    openai_available = True
-except ImportError:
-    st.error("Error: La librería 'openai' no está instalada. Algunas funcionalidades no estarán disponibles.")
-    openai_available = False
 
-# Inicialización del cliente OpenAI solo si está disponible
-if openai_available:
-    openai.api_key = st.secrets.get("OPENAI_API_KEY", "")
+# Streamed response emulator
+def response_generator():
+    response = random.choice(
+        [
+            "Hello there! How can I assist you today?",
+            "Hi, human! Is there anything I can help you with?",
+            "Do you need help?",
+        ]
+    )
+    for word in response.split():
+        yield word + " "
+        time.sleep(0.05)
+
+
+st.title("Simple chat")
+
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+# Accept user input
+if prompt := st.chat_input("What is up?"):
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    # Display user message in chat message container
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    # Display assistant response in chat message container
+    with st.chat_message("assistant"):
+        response = st.write_stream(response_generator())
+    # Add assistant response to chat history
+    st.session_state.messages.append({"role": "assistant", "content": response})
+
